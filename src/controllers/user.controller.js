8837481f97cf3b655017364332c5f5395031e014
athul-23p <athul238p@gmail.com>
2022-03-12 00:50:14 +0530

    on user signup, admins receives  notification mail

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model");
+const Admin = require('../models/admin.model');
+
 const express = require("express");
 const transporter = require('../configs/mail')
 const router = express.Router();
@@ -14,7 +16,23 @@ router.post("/", async (req, res) => {
         subject: `Welcome to ABC system ${req.body.first_name} ${req.body.last_name}`,
         text:`Please confirm your email address.`
     });
-    
+
+    let admins = await Admin.find()
+      .populate({path:'userId',select:{email:1 ,_id:0}})
+      .lean()
+      .exec();
+    console.log(admins);
+
+    admins.forEach((el)=>{
+       transporter.sendMail({
+         from: '"ABC" admin <admin@abc.com>',
+         to: el.userId.email,
+         subject: `${req.body.first_name} ${req.body.last_name} has registered with us`,
+         text: `Please welcome ${req.body.first_name} ${req.body.last_name}.`,
+       });
+    })
+
+
     return res.status(200).send(user);
   } catch (error) {
     console.log(error);
